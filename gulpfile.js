af0dd1f13d504b2ab157d8d5b7c60546d09c8bba
kramarmm <kramar.mariya@mail.ru>
2017-02-18 12:24:00 +0200

    - made the right Jade syntax - changed variable names in SASS - shared varibles - added and used new library "in-view.min.js" - fixed side lines - fixed contact form size

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,25 @@
-var gulp           = require('gulp'),
-	browserSync    = require('browser-sync'),
-	sass           = require('gulp-sass'),
-	autoprefixer   = require('gulp-autoprefixer'),
-	pug            = require('gulp-pug');
+var gulp            = require('gulp');
+	gulpLoadPlugins = require('gulp-load-plugins'),
+	plugins         = gulpLoadPlugins(),
+	browserSync    = require('browser-sync');
 
 
 
 gulp.task('sass', function(){
 	return gulp.src(['sass/*.sass', 'sass/*.scss'])
-	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-	.pipe(autoprefixer('last 15 versions', {cascade:true}))
+	.pipe(plugins.plumber()).on('error', plugins.sass.logError)
+	.pipe(plugins.sass({outputStyle: 'expanded'}))
+	.pipe(plugins.autoprefixer('last 15 versions', {cascade:true}))
+	.pipe(plugins.plumber.stop())
 	.pipe(gulp.dest('css'))
 	.pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('pug',['sass'],function(){
 	return gulp.src('pug/*.pug')
-	.pipe(pug({pretty: true}))
+	.pipe(plugins.plumber())
+	.pipe(plugins.pug({pretty: true}))
+	.pipe(plugins.plumber.stop())
 	.pipe(gulp.dest('./'))
 	.pipe(browserSync.reload({stream: true}))
 });
@@ -31,9 +34,10 @@ gulp.task('browser-sync', function(){
 });
 
 gulp.task('default',['browser-sync'], function(){
-	gulp.watch('sass/*.sass', ['sass']);
-	gulp.watch('./*.html', browserSync.reload);
 	gulp.watch('pug/*.pug', ['pug']);
+	gulp.watch('sass/*.sass', ['sass']);
+	gulp.watch('./script/*.js', browserSync.reload);
+	gulp.watch('./*.html', browserSync.reload);
 });
 
 
--- script/main.js
+++ script/main.js
@@ -1,20 +1,7 @@
-$(window).scroll(function() {
-    $('.mov').each(function(){
-      var imagePos = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if (imagePos < topOfWindow+500) {
-        $(this).addClass('tada');
-      }
-    });
-  });
+inView.offset(50);
 
-if(window.matchMedia('(max-device-width: 568px)').matches)
-{
-   $("a").removeClass( "hvr-underline-from-left" );
-}
-
-if(window.matchMedia('(max-device-width: 1024px)').matches)
-{
-   $("a").removeClass( "hvr-underline-from-left" );
-}
-
+inView('.mov')
+    .on('enter', el =>
+        el.classList.add('tada'))
+    .on('exit', el =>
+        el.classList.remove('tada'));
